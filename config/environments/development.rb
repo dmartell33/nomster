8cdf342ac776d6ea6ee1a0c7217bf490259d9dff
Dedrick Harrington <dedrick33@live.com>
2019-11-03 17:27:45 +0000

    Carousel photos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-# config.action_mailer.delivery_method = :test
+# config.action_mailer.delivery_method = :smtp
 
 # config.action_mailer.smtp_settings = {
 #   address: "smtp.gmail.com",
@@ -9,7 +9,7 @@ Rails.application.configure do
 #   enable_starttls_auto: true,
 #   user_name: ENV['GMAIL_ADDRESS'],
 #   password: ENV['GMAIL_PASSWORD']
-  }
+# }
 
 
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,17 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'nomster-dedrick-harrington2.herokuapp.com' }
+ 
+ ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  } 
+
+
+ config.action_mailer.default_url_options = { host: 'nomster-dedrick-harrington.herokuapp.com' }
 
 
 
